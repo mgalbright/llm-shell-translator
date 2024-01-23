d3fc38e05406b3153551ac9fd1266384abe33d81
Michael Albright <13350377+mgalbright@users.noreply.github.com>
2024-01-23 16:13:36 -0600

    refactor code to use function api in chat.completions
    using function feature in chat.completions
    will provide better reliability by forcing the
    model's output to match the json signature
    we specify
    
    see e.g.
    https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models

--- llm-assistant.py
+++ llm-assistant.py
@@ -12,34 +12,73 @@ MODEL_NAME = "gpt-3.5-turbo"
 
 MAX_TOKENS=256
 
-BASH_TRANSLATE_PROMPT = """You translate english requests into working bash shell commands.  
-You will output the command and metadata in a json structure like this: 
-{{command:"<COMMAND>", confident:<CONFIDENCE>, risk:<RISK>}}
-
-You will replace <COMMAND> with the valid, working bash shell command that solves the request.  
-
-If the command is possibly risky to the linux system, such as potentially causing a loss 
-of files or user accounts, or otherwise harming the linux system, replace <RISK> with True, otherwise replace <RISK> with False.
-
-Only generate bash commands that you are highly-confident will run correctly. 
-Do not invent commands that do not exist.  If there is no way to solve the question 
-with simple bash commands, list "" for the command.  
-
-If you are not highly confident your command is a correct bash command, list "" 
-for the command and replace <CONFIDENCE> with False. If you are highly-confident your command is a 
-valid, working bash command, replace <CONFIDENCE> with True.
-
-Do not output any response besides json that conforms to the template provided.
+BASH_EXPLAIN_PROMPT = """You are an AI that is given a linux bash command, and 
+you explain what it does in English.  If it is not a bash command, respond 
+'sorry, idk'.  Do not invent answers. Only return answers that you are very 
+confident are correct.  If you are unsure what a command does, respond 
+'sorry, idk'.  Do not provide any answer that is not either (1) an explanation 
+of a shell command or (2) 'sorry, idk'.
 """
 
-BASH_EXPLAIN_PROMPT = """You are an AI that is given a complicated linux bash command, and you explain what it does
-in English.  If it is not a bash command, respond 'sorry, idk'.  
-Do not invent answers. Only return answers that you are very confident are correct.  If you are unsure what a command
-does, respond 'sorry, idk'.  Do not provide any answer that is not either (1) an exaplanation of a shell command or
-(2) 'sorry, idk'.
+BASH_TRANSLATE_PROMPT = """You receive a task specified by natural language 
+that must be accomplished in the linux terminal. You translate that task into 
+valid bash shell commands that accomplishes the task.  
+
+Generate a valid bash shell command that will run without errors. 
+Do not invent bash commands that do not exist.  
+
+If there is no way to solve the task with bash commands, return
+"" for command, True for confident, and False for risky.
+
+If you are not highly confident your command is a valid bash command, return "" 
+for command and False for confident. If you are highly-confident your command is 
+a valid, working bash command, return True for confident.
+
+If the task is unrelated to linux or bash, return "" for command, True for 
+confident, and False for risky.
 """
+
+#-------------------------------------------------------------------------------
+#In translate tasks, we will use the function api feature in chat.completions 
+#to force chatgpt's output to conform to valid arguments to print_bash_command()
+
+#Must define the signature of the function print_bash_command(). See:
+# * https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
+# * https://platform.openai.com/docs/api-reference/chat/create
+# * https://json-schema.org/understanding-json-schema/reference/type
+
+FUNCTION_SPECIFICATIONS = [
+  {
+    "name": "print_bash_command",
+    "description": "translate a task in natural language to a valid bash command, with info about riskiness of the command and confidence that the command is valid.",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "command":{
+          "type": "string",
+          "description": "valid linux bash shell command"
+        },
+        
+        "confident":{
+          "type": "boolean",
+          "description": "Confidence that translated command is a valid bash shell command: True if the model is confident that command is a valid bash command that solves the task, else False"
+        },
+        
+        "risky":{
+          "type": "boolean",
+          "description": "Riskiness of the command: True if the bash command is potentially risky to linux systems (e.g. might cause a loss of user accounts or data), else False"
+        }
+        
+      },
+      "required": ["command", "confident", "risky"]
+    }
+  }
+]
+
+#-------------------------------------------------------------------------------
 
 def query_openai_prompt(client, system_prompt, user_prompt):
+  """Ask chatgpt questions with text prompt from user"""
   chat_completion = client.chat.completions.create(
     messages=[
         {"role": "system", "content": system_prompt},
@@ -49,17 +88,40 @@ def query_openai_prompt(client, system_prompt, user_prompt):
   )
   return chat_completion
 
+def query_openai_function_api(client, system_prompt, user_prompt, function_specifications):
+  """Ask chatgpt a prompt whose response must take the form of valid arguments 
+  that can be passed to a function. Uses the function api.
+  
+  See e.g.
+  * https://platform.openai.com/docs/api-reference/chat/create
+  * https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
+  """
+  chat_completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ],
+    model=MODEL_NAME,
+    functions=function_specifications,
+    
+    #force chatgpt to create a response conforming to the signature of this function
+    #if use 'auto' it can chose between forming arugments or just a textual response (e.g. to ask for clarification)
+    function_call={"name": function_specifications[0]['name']}
+  )
+  return chat_completion
 
 def translate_to_bash(client, user_question):
-  return query_openai_prompt(client, BASH_TRANSLATE_PROMPT, user_question)
+  return query_openai_function_api(client, BASH_TRANSLATE_PROMPT, user_question, FUNCTION_SPECIFICATIONS)
 
 def explain_bash(client, user_question):
   return query_openai_prompt(client, BASH_EXPLAIN_PROMPT, user_question)
 
 #-------------
 
-def extract_json(response_text):
-  blank_response = {"command": "", "confident": True, "risk": False}
+def clean_function_json(response_text):
+  """maybe not necessary but verify /clean response in case it is malformed"""
+
+  blank_response = {"command": "", "confident": True, "risky": False}
   
   try:
     api_response = json.loads(response_text)
@@ -67,21 +129,22 @@ def extract_json(response_text):
     print(f"debug: incorrect response returned: {response_text}")
     api_response = blank_response
 
-  if not ('risk' in api_response.keys() and 'confident' in api_response.keys() and 'command' in api_response.keys()):
+  if not ('risky' in api_response.keys() and 'confident' in api_response.keys() and 'command' in api_response.keys()):
     print(f"debug: malformed returned: {response_text}")
     api_response = blank_response
 
   return api_response
 
-def print_warnings(response_json):
-  if response_json['risk'] == True:
+def print_bash_command(command: str, confident: bool, risky: bool):
+  """prints a bash command along with the model's confidence and whether or not the command is risky to a linux system"""
+  if risky:
     print("CAUTION: this command could be risky to your system")
     
-  if response_json['confident'] == False:
-    if len(response_json['command']) > 0:
+  if not confident:
+    if len(command) > 0:
       print("--caution: I'm not certain, but here's a guess:")
 
-  print(response_json['command'])
+  print(command)
 
 #-----------------------------------------
   
@@ -104,8 +167,7 @@ if __name__ == '__main__':
 
     elif args.translate:
       chat_completion = translate_to_bash(client, args.prompt)
-      chat_completion.choices[0].message.content
-      json_response = extract_json(chat_completion.choices[0].message.content)
+      json_response = clean_function_json(chat_completion.choices[0].message.function_call.arguments)
 
-      print_warnings(json_response)
+      print_bash_command(**json_response)
   
